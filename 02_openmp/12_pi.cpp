b9c5bdfad225c42a1b207f55514420362f95bbaa
yutayamamura <yamamura.y.3fa1@m.isct.ac.jp>
2026-04-22 23:32:14 +0900

    add timing and parallel/serial switch to pi calculation

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,14 +1,19 @@
 #include <cstdio>
+#include <cstdlib>
 #include <omp.h>
 
-int main() {
+int main(int argc, char **argv) {
   int n = 10;
+  int parallel = (argc > 1) ? atoi(argv[1]) : 1;
   double dx = 1. / n;
   double pi = 0;
-#pragma omp parallel for reduction(+:pi)
+  double t0 = omp_get_wtime();
+#pragma omp parallel for reduction(+:pi) if(parallel)
   for (int i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
   }
+  double t1 = omp_get_wtime();
   printf("%17.15f\n",pi);
+  printf("time: %f s (%s)\n", t1 - t0, parallel ? "parallel" : "serial");
 }
