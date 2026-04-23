53c333222c207a2b39b491ef14f839c9784e1cc7
yutayamamura <yamamura.y.3fa1@m.isct.ac.jp>
2026-04-23 22:40:02 +0900

    add OpenMP parallelization to bucket sort using parallel prefix sum

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 int main() {
+  // Create a random array
   int n = 50;
   int range = 5;
   std::vector<int> key(n);
@@ -12,18 +13,38 @@ int main() {
   }
   printf("\n");
 
+  // Put the array into buckets
   std::vector<int> bucket(range,0); 
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
   std::vector<int> offset(range,0);
+  // Generate bucket offsets (exclusive prefix sum)
   for (int i=1; i<range; i++) 
     offset[i] = offset[i-1] + bucket[i-1];
-  for (int i=0; i<range; i++) {
-    int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
-    }
+  // Reconstruct the sorted array
+//  for (int i=0; i<range; i++) {
+//    int j = offset[i];
+//    for (; bucket[i]>0; bucket[i]--) {
+//      key[j++] = i;
+//    }
+//  }
+  std::vector<int> mark(n,0);
+  for (int i=1; i<range; i++)
+    mark[offset[i]] = 1;
+
+  std::vector<int> b(n);
+#pragma omp parallel
+  for (int j=1; j<n; j<<=1) {
+#pragma omp for
+    for (int i=0; i<n; i++)
+      b[i] = mark[i];
+#pragma omp for
+    for (int i=j; i<n; i++)
+      mark[i] += b[i-j];
   }
+#pragma omp parallel for
+  for (int i=0; i<n; i++)
+    key[i] = mark[i];
 
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
