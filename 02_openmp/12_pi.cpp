2eb6a5c4f8f24c22a996d401949d4845767f7f9c
yutayamamura <yamamura.y.3fa1@m.isct.ac.jp>
2026-04-23 11:46:05 +0900

    add OpenMP task parallelization to merge sort and reformat pi calculation

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -3,13 +3,15 @@
 #include <omp.h>
 
 int main(int argc, char **argv) {
+  int parallel = (argc > 1) ? atoi(argv[1]) : 1;
+
   // Serial outperforms parallel for n < 10^5 due to thread overhead (tested at n = 10^1, 10^2, ...).
   int n = 10;
-  int parallel = (argc > 1) ? atoi(argv[1]) : 1;
   double dx = 1. / n;
   double pi = 0;
+
   double t0 = omp_get_wtime();
-#pragma omp parallel for reduction(+:pi) if(parallel)
+  #pragma omp parallel for reduction(+:pi) if(parallel)
   for (int i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -6,7 +6,7 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
   std::vector<int> tmp(end-begin+1);
   int left = begin;
   int right = mid+1;
-  for (int i=0; i<tmp.size(); i++) { 
+  for (int i=0; i<tmp.size(); i++) {
     if (left > mid)
       tmp[i] = vec[right++];
     else if (right > end)
@@ -14,22 +14,31 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
     else if (vec[left] <= vec[right])
       tmp[i] = vec[left++];
     else
-      tmp[i] = vec[right++]; 
+      tmp[i] = vec[right++];
   }
-  for (int i=0; i<tmp.size(); i++) 
+  for (int i=0; i<tmp.size(); i++)
     vec[begin++] = tmp[i];
 }
 
 void merge_sort(std::vector<int>& vec, int begin, int end) {
   if(begin < end) {
     int mid = (begin + end) / 2;
+
+    #pragma omp task shared(vec)
     merge_sort(vec, begin, mid);
+
+    #pragma omp task shared(vec)
     merge_sort(vec, mid+1, end);
+
+    #pragma omp taskwait
+
     merge(vec, begin, mid, end);
   }
 }
 
-int main() {
+int main(int argc, char **argv) {
+  int parallel = (argc > 1) ? atoi(argv[1]) : 1;
+
   int n = 20;
   std::vector<int> vec(n);
   for (int i=0; i<n; i++) {
@@ -37,9 +46,16 @@ int main() {
     printf("%d ",vec[i]);
   }
   printf("\n");
-  merge_sort(vec, 0, n-1);
+
+  double t0 = omp_get_wtime();
+  #pragma omp parallel{
+    #pragma omp single
+    merge_sort(vec, 0, n-1);
+  }
+  double t1 = omp_get_wtime();
   for (int i=0; i<n; i++) {
     printf("%d ",vec[i]);
   }
   printf("\n");
+  printf("time: %f s (%s)\n", t1 - t0, parallel ? "parallel" : "serial");
 }
